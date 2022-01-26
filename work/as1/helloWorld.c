0fddccd2b6316bad3bc3c594c7764b546d5abfe9
Avneet <avneetsekhonfp@gmail.com>
2022-01-25 23:14:44 -0800

    LED response to correct/incorrect

--- work/as1/helloWorld.c
+++ work/as1/helloWorld.c
@@ -5,13 +5,165 @@
 #include <sys/epoll.h>
 
 #define DA_TRIGGER_FILE_NAME_HERE "/sys/class/leds/beaglebone:green:usr0/trigger"
+#define LED1_TRIGGER_FILE "/sys/class/leds/beaglebone:green:usr1/trigger"
+#define LED2_TRIGGER_FILE "/sys/class/leds/beaglebone:green:usr2/trigger"
 #define SECOND_TRIGGER_FILE_NAME_HERE "/sys/class/leds/beaglebone:green:usr3/trigger"
+
+#define FIRST_LED_BRIGHTNESS_FILE "/sys/class/leds/beaglebone:green:usr0"
+#define SECOND_LED_BRIGHTNESS_FILE "/sys/class/leds/beaglebone:green:usr1"
+#define THIRD_LED_BRIGHTNESS_FILE "/sys/class/leds/beaglebone:green:usr2"
+#define FOURTH_LED_BRIGHTNESS_FILE "/sys/class/leds/beaglebone:green:usr3"
+
 #define JOYSTICK_PRESSED_UP "/sys/class/gpio/gpio26/value"
 #define JOYSTICK_PRESSED_DOWN "/sys/class/gpio/gpio46/value"
 #define JOYSTICK_PRESSED_LEFT "/sys/class/gpio/gpio65/value"
 #define JOYSTICK_PRESSED_RIGHT "/sys/class/gpio/gpio47/value"
 
-//read from GPIO file
+
+// Brightness off
+void brightnessClose(char *fileName)
+{	
+	char brightnessPath[1024];
+	strcpy(brightnessPath, fileName);
+	strcat(brightnessPath, "/brightness");
+	
+	FILE *pLedBrightnessFile = fopen(brightnessPath, "w");
+	
+	int charWritten = fprintf(pLedBrightnessFile, "0");
+	if (charWritten <= 0) { 
+		printf("ERROR WRITING DATA");
+		exit(1);
+	}
+	fclose(pLedBrightnessFile);
+}
+
+
+// Brightness on
+void brightnessOpen(char *fileName)
+{	
+	char brightnessPath[1024];
+	strcpy(brightnessPath, fileName);
+	strcat(brightnessPath, "/brightness");
+	
+	// Write into led files
+	FILE *pLedBrightnessFile = fopen(brightnessPath, "w");
+		
+	if (pLedBrightnessFile == NULL) 
+	{
+		printf("ERROR OPENING %s.", brightnessPath);
+		exit(1);
+	}
+	int charWritten = fprintf(pLedBrightnessFile, "100");
+	if (charWritten <= 0) { 
+		printf("ERROR WRITING DATA");
+		exit(1);
+	}
+	fclose(pLedBrightnessFile);
+}
+
+void flashLED(int numFlash, long sec, long nsec)
+{
+	long seconds = sec;
+   	long nanoseconds = nsec;
+   	struct timespec reqDelay = {seconds, nanoseconds};
+   	
+   	for (int i = 0; i < numFlash; i++)
+   	{
+		brightnessOpen(FIRST_LED_BRIGHTNESS_FILE);
+		brightnessOpen(SECOND_LED_BRIGHTNESS_FILE);
+		brightnessOpen(THIRD_LED_BRIGHTNESS_FILE);
+		brightnessOpen(FOURTH_LED_BRIGHTNESS_FILE);
+		
+		// Sleep
+   		nanosleep(&reqDelay, (struct timespec *) NULL);
+   		
+		brightnessClose(FIRST_LED_BRIGHTNESS_FILE);
+		brightnessClose(SECOND_LED_BRIGHTNESS_FILE);
+		brightnessClose(THIRD_LED_BRIGHTNESS_FILE);
+		brightnessClose(FOURTH_LED_BRIGHTNESS_FILE);
+		
+		// Sleep
+   		nanosleep(&reqDelay, (struct timespec *) NULL);	
+   	}
+}
+
+//Turn on LED0 "UP"
+void turnOnUpLed(char *fileName, char *secondFileName)
+{
+	//Write into led files and turn off LED3
+	FILE *pLedTriggerFile2 = fopen(fileName, "w");
+
+	if (pLedTriggerFile2 == NULL) 
+	{
+		printf("ERROR OPENING %s.", fileName);
+		exit(1);
+	}
+
+	int charWritten2 = fprintf(pLedTriggerFile2, "none");
+	if (charWritten2 <= 0) { 
+		printf("ERROR WRITING DATA");
+		exit(1);
+	}
+
+	fclose(pLedTriggerFile2);
+	
+	//Write into led files turn on LED0
+	FILE *pLedTriggerFile = fopen(secondFileName, "w");
+
+	if (pLedTriggerFile == NULL) 
+	{
+		printf("ERROR OPENING %s.", secondFileName);
+		exit(1);
+	}
+
+	int charWritten = fprintf(pLedTriggerFile, "default-on");
+	if (charWritten <= 0) { 
+		printf("ERROR WRITING DATA");
+		exit(1);
+	}
+	
+	fclose(pLedTriggerFile);
+}
+
+//Turn on LED3 "DOWN"
+void turnOnDownLed(char *fileName, char *secondFileName)
+{
+	//Write into led files and turn off LED0
+	FILE *pLedTriggerFile = fopen(fileName, "w");
+
+	if (pLedTriggerFile == NULL) 
+	{
+		printf("ERROR OPENING %s.", fileName);
+		exit(1);
+	}
+
+	int charWritten = fprintf(pLedTriggerFile, "none");
+	if (charWritten <= 0) { 
+		printf("ERROR WRITING DATA");
+		exit(1);
+	}
+
+	fclose(pLedTriggerFile);
+	
+	//Write into led files and turn on LED3
+	FILE *pLedTriggerFile2 = fopen(secondFileName, "w");
+
+	if (pLedTriggerFile2 == NULL) 
+	{
+		printf("ERROR OPENING %s.", secondFileName);
+		exit(1);
+	}
+
+	int charWritten2 = fprintf(pLedTriggerFile2, "default-on");
+	if (charWritten2 <= 0) { 
+		printf("ERROR WRITING DATA");
+		exit(1);
+	}
+
+	fclose(pLedTriggerFile2);
+}
+
+//Read from GPIO file
 int readFromFileToScreen(char *fileName, int updateScore)
 {
    int result = 1;
@@ -26,7 +178,7 @@ int readFromFileToScreen(char *fileName, int updateScore)
 	exit(-1);
    }
    
-   // Sleep 0.3 seconds
+   // Sleep
    nanosleep(&reqDelay, (struct timespec *) NULL);
    
    // Read string (line)
@@ -40,9 +192,11 @@ int readFromFileToScreen(char *fileName, int updateScore)
    if (result == 10)
    {
    	printf("Correct!\n");
+   	flashLED(1, 0, 100000000);
    	updateScore+=1;
    } else {
-   	printf("Incorrect ;(\n");
+   	printf("Incorrect :(\n");
+   	flashLED(5, 0, 100000000);
    }
    
    // Close
@@ -53,7 +207,7 @@ int readFromFileToScreen(char *fileName, int updateScore)
    return updateScore; 
 }
 
-//read from GPIO file to EXIT
+//Read from GPIO file to EXIT
 void readFromFileToExit(char *fileName, int finalScore, int finalRounds)
 {
    int result = 1;
@@ -68,7 +222,7 @@ void readFromFileToExit(char *fileName, int finalScore, int finalRounds)
 	exit(-1);
    }
    
-   // Sleep 0.3 seconds
+   // Sleep
    nanosleep(&reqDelay, (struct timespec *) NULL);
    
    // Read string (line)
@@ -82,7 +236,7 @@ void readFromFileToExit(char *fileName, int finalScore, int finalRounds)
    if (result == 10)
    {
    	printf("Your final score was (%d / %d)\n", finalScore, finalRounds);
-   	printf("Thank you for playing\n");
+   	printf("Thank you for playing!\n");
    	exit(-1);
    } 
    
@@ -106,53 +260,13 @@ int main(int argc, char* args[]) {
    
    printf("Press joystick; Score: (%d / %d)\n", score, rounds);
    
-   long seconds = 1;
-   long nanoseconds = 100000000;
-   struct timespec reqDelay = {seconds, nanoseconds};
-   
-   int n = 1;
-   while (n < 10)
+   while (1)
    {   
 	   int randNum = rand() % 2;
 	   
 	   if (randNum % 2 == 0) //Turn on LED0
 	   {
-		//Write into led files and turn off LED3
-		FILE *pLedTriggerFile2 = fopen(SECOND_TRIGGER_FILE_NAME_HERE, "w");
-
-		if (pLedTriggerFile2 == NULL) 
-		{
-			printf("ERROR OPENING %s.", SECOND_TRIGGER_FILE_NAME_HERE);
-			exit(1);
-		}
-		//printf("File opened!\n");
-
-		int charWritten2 = fprintf(pLedTriggerFile2, "none");
-		if (charWritten2 <= 0) { 
-			printf("ERROR WRITING DATA");
-			exit(1);
-		}
-
-		fclose(pLedTriggerFile2);
-		
-		//Write into led files turn on LED0
-		FILE *pLedTriggerFile = fopen(DA_TRIGGER_FILE_NAME_HERE, "w");
-
-		if (pLedTriggerFile == NULL) 
-		{
-			printf("ERROR OPENING %s.", DA_TRIGGER_FILE_NAME_HERE);
-			exit(1);
-		}
-		//printf("File opened!\n");
-
-		int charWritten = fprintf(pLedTriggerFile, "default-on");
-		if (charWritten <= 0) { 
-			printf("ERROR WRITING DATA");
-			exit(1);
-		}
-		
-		fclose(pLedTriggerFile);
-		
+		turnOnUpLed(SECOND_TRIGGER_FILE_NAME_HERE, DA_TRIGGER_FILE_NAME_HERE);
 		score = readFromFileToScreen(JOYSTICK_PRESSED_UP, score);
 		readFromFileToExit(JOYSTICK_PRESSED_LEFT, score, rounds);
 		readFromFileToExit(JOYSTICK_PRESSED_RIGHT, score, rounds);
@@ -161,51 +275,12 @@ int main(int argc, char* args[]) {
 	   
 	   if (randNum % 2 == 1) //Turn on LED3
 	   {
-		//Write into led files and turn off LED0
-		FILE *pLedTriggerFile = fopen(DA_TRIGGER_FILE_NAME_HERE, "w");
-
-		if (pLedTriggerFile == NULL) 
-		{
-			printf("ERROR OPENING %s.", DA_TRIGGER_FILE_NAME_HERE);
-			exit(1);
-		}
-		//printf("File opened!\n");
-
-		int charWritten = fprintf(pLedTriggerFile, "none");
-		if (charWritten <= 0) { 
-			printf("ERROR WRITING DATA");
-			exit(1);
-		}
-
-		fclose(pLedTriggerFile);
-		
-		//Write into led files and turn on LED3
-		FILE *pLedTriggerFile2 = fopen(SECOND_TRIGGER_FILE_NAME_HERE, "w");
-
-		if (pLedTriggerFile2 == NULL) 
-		{
-			printf("ERROR OPENING %s.", SECOND_TRIGGER_FILE_NAME_HERE);
-			exit(1);
-		}
-		//printf("File opened!\n");
-
-		int charWritten2 = fprintf(pLedTriggerFile2, "default-on");
-		if (charWritten2 <= 0) { 
-			printf("ERROR WRITING DATA");
-			exit(1);
-		}
-
-		fclose(pLedTriggerFile2);
-		
+		turnOnDownLed(DA_TRIGGER_FILE_NAME_HERE, SECOND_TRIGGER_FILE_NAME_HERE);
 		score = readFromFileToScreen(JOYSTICK_PRESSED_DOWN, score);
 		readFromFileToExit(JOYSTICK_PRESSED_LEFT, score, rounds);
 		readFromFileToExit(JOYSTICK_PRESSED_RIGHT, score, rounds);
 	   }
 	    
-	   // Sleep 0.3 seconds
-	   nanosleep(&reqDelay, (struct timespec *) NULL);
-	   //printf("running: %d\n", n); 
-	   n++;
 	   rounds++;
 	   printf("Press joystick; Score: (%d / %d)\n", score, rounds);
    }
